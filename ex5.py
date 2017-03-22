7e99e10714fe09d91cbab4aa2d7ff4370d3f1819
arvindkumarb <madhrasy@gmail.com>
2017-03-22 07:30:52 +0530

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Arvind Kumar B.'
+my_age = 24 # little lie
+my_height = 180 # cms
+my_weight = 71 # kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kg heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# tricky
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
